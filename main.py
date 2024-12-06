53d3267b57583c5652a4f19684b605d898560a38
Senor_Vostok <tabulgatin1@gmail.com>
2024-12-06 21:24:26 +0500

    поисковая строка и анимации интерфейса

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ def calculate_popularity(x):
     budget_places = x[4]
     paid_places = x[5]
     price = manager.get_data('SELECT price FROM po_paid WHERE code=? AND profile=?', (x[0], x[2]))[0][0]
-    average_score = manager.get_data(
-        'SELECT average FROM sl_years WHERE code=? AND name=?', (x[0], x[1])
-    )[0][0]
-    W1 = 1.5
-    W2 = 1.0
-    W3 = 0.1
-    W4 = 0.05
-    popularity = (
-        (W1 * average_score + W2 * price + W4 * paid_places) /
-        (1 + math.exp(-(W3 * budget_places)))
-    )
+    average_score = manager.get_data('SELECT average FROM sl_years WHERE code=? AND name=?', (x[0], x[1]))[0][0]
+    w1, w2, w3, w4 = 1.5, 1.0, 0.1, 0.05
+    popularity = ((w1 * average_score + w2 * price + w4 * paid_places) / (1 + math.exp(-(w3 * budget_places))))
     return popularity
+
+
+def get_item(rule, params, exist):
+    item = manager.get_data(rule, params)[0][0]
+    if item == 0:
+        return exist
+    return item
 
 
 @app.route('/')
@@ -29,9 +28,9 @@ def testing():
     result = sorted(massive_areas, key=lambda x: calculate_popularity(x))
     result.reverse()
     data = [{"title": f"{i[0]}\\n{i[1]}", "description": f"Профиль: {i[2]}\\n\\nФакультет: {i[3]}\\n\\n"
-                                                         f"Минимальный средний балл: {manager.get_data('SELECT average FROM sl_years WHERE code=? AND name=?', (i[0], i[1]))[0][0]}\\n"
-                                                         f"Общее количество бюджетных мест: {i[4]}\\n"
-                                                         f"Стоимость обучения на платной основе: {manager.get_data('SELECT price FROM po_paid WHERE code=? AND (profile=? OR name=?)', (i[0], i[2], i[2]))[0][0]} р"} for i in result]
+             f"Минимальный средний балл: {get_item('SELECT average FROM sl_years WHERE code=? AND name=?', (i[0], i[1]), 'отсутствует')}\\n"
+             f"Общее количество бюджетных мест: {i[4]}\\n"
+             f"Стоимость обучения на платной основе: {manager.get_data('SELECT price FROM po_paid WHERE code=? AND (profile=? OR name=?)',(i[0], i[2], i[1]))[0][0]} р"} for i in result]
     return render_template('base.html', data=data)
 
 
